a62abf1908090fce7cc25f43f78e6bbc28577c13
toniopas <antoine.pasqualini31@gmail.com>
2026-04-07 13:03:12 +0200

    new

---
+++ FOOD/get_food.py
@@ -0,0 +1,17 @@
+from food import Aliment as Food
+import argparse
+
+import sys
+print("Running script...")
+
+parser = argparse.ArgumentParser("Food Informations")
+parser.add_argument('-f', '--food', help="your food name", default='tomate')
+
+# use the parser to get all the needed arguments
+args = parser.parse_args()
+
+# retrieve and display food infos
+food = Food()
+food.recuperer_infos(args.food)
+food.afficher_infos()
+food.sauvegarder_csv(f"{args.food}.csv")
